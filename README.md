c78eb1a7446905faaff83e2fd979bcad30f9d2f4
Euan <euank@euank.com>
2014-03-03 19:05:57 -0500

    Add different implementation

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ e.g. if you have some logs that you know are 5 minutes ahead, you could do
 and it would move the logs back 300 seconds in time.
 
 Public domain because I sure don't want to own this solely
+
+### Problems
+
+Known issues with timewarp are that it doesn't handle notices / join/part info
+correctly. This is a problem with the log-parsing library I used (with how I
+used it too).
+
+timeshift.rb does everything correctly as far as I know. Its usage is a little
+different because you give it an out template instead of an outdir e.g.
+`outdir/user_network\#channel_`, but it's otherwise the same.
---
+++ timeshift.rb
@@ -0,0 +1,40 @@
+
+if ARGV.length < 3
+  puts "Arguments: <seconds diff> <outdir> <in files ...>"
+  exit
+end
+
+second_diff = ARGV.shift.to_i
+out_template = ARGV.shift
+
+chat_lines = []
+
+ARGV.each do |in_file|
+  ymd = in_file.match(/(\d{4})(\d{2})(\d{2})\.log$/)
+
+  File.open(in_file, 'r') do |file|
+    file.each_line do |line|
+      time_text = line.match(/\[(\d\d):(\d\d):(\d\d)\](.*)/)
+      chat_lines << {time: Time.new(ymd[1].to_i,
+                                    ymd[2].to_i,
+                                    ymd[3].to_i,
+                                    time_text[1].to_i,
+                                    time_text[2].to_i,
+                                    time_text[3].to_i) + second_diff,
+                    text: time_text[4]}
+    end
+  end
+end
+
+files = {}
+
+chat_lines.each do |chat_line|
+  files[chat_line[:time].strftime("%Y%m%d")] ||= []
+  files[chat_line[:time].strftime("%Y%m%d")] << chat_line
+end
+
+files.each_pair do |date, c_lines|
+  File.open(out_template + date + ".log", 'w') do |file|
+    file.write(c_lines.map{|cl| "[#{cl[:time].strftime("%H:%M:%S")}]#{cl[:text]}"}.join("\n"))
+  end
+end
